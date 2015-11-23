53dee3e1b7c1c5f2dc4a6bbc718e4a7f5c9d5fd9
Nikita Volkov <nikita.y.volkov@mail.ru>
2015-11-23 07:58:23 +0300

    Reimplement "monadBaseControlTransformerInstance" and "monadBaseTransformerInstance"

--- library/NewtypeDeriving/Rendering.hs
+++ library/NewtypeDeriving/Rendering.hs
@@ -5,7 +5,7 @@ import Control.Monad.Base
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Class
 import NewtypeDeriving.TH
-import Language.Haskell.TH
+import Language.Haskell.TH hiding (classP)
 
 
 monadTransInstance 
@@ -24,17 +24,49 @@ monadTransInstance transformerType conName layersCount =
     liftExp =
       foldl' composeExp (ConE conName) $ replicate layersCount (VarE 'lift)
 
+-- |
+-- Produces a declaration in the spirit of the following:
+-- @
+-- instance MonadBase b m => MonadBase b ($(pure transformerT) m) where
+--   liftBase =
+--     $(conE conName) . liftBase
+-- @
 monadBaseTransformerInstance
   :: Type
   -> Name
   -> Dec
-monadBaseTransformerInstance transformerType conName =
-  head $ purifyQ $
-  [d|
-    instance MonadBase b m => MonadBase b ($(pure transformerType) m) where
-      liftBase =
-        $(conE conName) . liftBase
-  |]
+monadBaseTransformerInstance transformerT conName =
+  InstanceD cxt type_ decs
+  where
+    baseMonadName =
+      mkName "b"
+    mainMonadName =
+      mkName "m"
+    baseMonadT =
+      VarT baseMonadName
+    mainMonadT =
+      VarT mainMonadName
+    cxt =
+      [pred]
+      where
+        pred =
+          classP ''MonadBase [baseMonadT, mainMonadT]
+    type_ =
+      foldl1 AppT [ConT ''MonadBase, baseMonadT, AppT transformerT mainMonadT]
+    decs =
+      [liftBaseD]
+      where
+        liftBaseD =
+          FunD 'liftBase [clause]
+          where
+            clause =
+              Clause [] body []
+              where
+                body =
+                  NormalB exp
+                  where
+                    exp =
+                      composeExp (ConE conName) (VarE 'liftBase)
 
 monadTransControlInstance 
   :: Type   -- ^ Type of Newtype
@@ -79,17 +111,72 @@ monadTransControlInstance transformerType conName layers =
     stType =
       foldl' (flip AppT) (VarT stArgName) $ map (AppT (ConT ''StT)) layers
 
+-- |
+-- Produces a declaration in the spirit of the following:
+-- @
+-- instance MonadBaseControl b m => MonadBaseControl b ($(pure transformerT) m) where
+--   type StM ($(pure transformerT) m) a = ComposeSt $(pure transformerT) m a
+--   liftBaseWith = defaultLiftBaseWith
+--   restoreM = defaultRestoreM
+-- @
 monadBaseControlTransformerInstance
   :: Type
   -> Dec
-monadBaseControlTransformerInstance t =
-  head $ purifyQ $
-  [d|
-    instance MonadBaseControl b m => MonadBaseControl b ($(pure t) m) where
-      type StM ($(pure t) m) a = ComposeSt $(pure t) m a
-      liftBaseWith = defaultLiftBaseWith
-      restoreM = defaultRestoreM
-  |]
+monadBaseControlTransformerInstance transformerT =
+  InstanceD cxt type_ decs
+  where
+    baseMonadName =
+      mkName "b"
+    mainMonadName =
+      mkName "m"
+    resultName =
+      mkName "a"
+    baseMonadT =
+      VarT baseMonadName
+    mainMonadT =
+      VarT mainMonadName
+    resultT =
+      VarT resultName
+    cxt =
+      [pred]
+      where
+        pred =
+          classP ''MonadBaseControl [baseMonadT, mainMonadT]
+    type_ =
+      AppT (AppT (ConT ''MonadBaseControl) baseMonadT) (AppT transformerT mainMonadT)
+    decs =
+      [stMD, liftBaseWithD, restoreMD]
+      where
+        stMD =
+          TySynInstD ''StM eqn
+          where
+            eqn =
+              TySynEqn [AppT transformerT mainMonadT, resultT] rhsT
+              where
+                rhsT =
+                  foldl1 AppT [ConT ''ComposeSt, transformerT, mainMonadT, resultT]
+        liftBaseWithD =
+          FunD 'liftBaseWith [clause]
+          where
+            clause =
+              Clause [] body []
+              where
+                body =
+                  NormalB exp
+                  where
+                    exp =
+                      VarE 'defaultLiftBaseWith
+        restoreMD =
+          FunD 'restoreM [clause]
+          where
+            clause =
+              Clause [] body []
+              where
+                body =
+                  NormalB exp
+                  where
+                    exp =
+                      VarE 'defaultRestoreM
 
 composeExp :: Exp -> Exp -> Exp
 composeExp a b =
--- library/NewtypeDeriving/TH.hs
+++ library/NewtypeDeriving/TH.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module NewtypeDeriving.TH where
 
 import BasePrelude
@@ -9,3 +10,13 @@ purifyQ = unsafePerformIO . runQ
 
 tryToReify :: Name -> Q (Maybe Info)
 tryToReify n = recover (return Nothing) (fmap Just $ reify n) 
+
+{-# INLINE classP #-}
+classP :: Name -> [Type] -> Pred
+#if MIN_VERSION_template_haskell(2,10,0)
+classP n tl =
+  foldl AppT (ConT n) tl
+#else
+classP =
+  ClassP
+#endif
